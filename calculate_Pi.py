00ba44abddf8d4d0873bc356748aa8fc31b42193
glofiniteJun <39223887+glofiniteJun@users.noreply.github.com>
2019-11-04 23:11:57 +0900

    final codes and reports

---
+++ calculate_Pi.py
@@ -0,0 +1,41 @@
+
+def calcPi():  
+
+    q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+
+    counter = 0
+
+    while 1 :
+            if 4 * q + r - t < n * t:
+                    yield n
+                    if counter == 0:
+                            yield '.'
+                    counter += 1
+                    nr = 10 * (r - n * t)
+                    n = ((10 * (3 * q + r)) // t) - 10 * n
+                    q *= 10
+                    r = nr
+            else:
+                    nr = (2 * q + r) * l
+                    nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
+                    q *= k
+                    t *= l
+                    l += 2
+                    k += 1
+                    n = nn
+                    r = nr
+
+
+def main():  
+
+    pi_digits = calcPi()
+
+    i = 0
+
+    for d in pi_digits:
+            i += 1
+            if i == 40:
+                i = 0
+
+if __name__ == '__main__':
+    main()
